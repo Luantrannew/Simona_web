2deb70cbf7706e42e6b7cf8e0971dd651df2d9ce
Luantrannew <tranduyluantranduyluan>
2024-08-13 14:00:23 +0700

    chatbot complete

--- record/consumers.py
+++ record/consumers.py
@@ -1,3 +1,4 @@
+import re
 import json
 from asgiref.sync import sync_to_async
 from asgiref.sync import async_to_sync
@@ -21,61 +22,61 @@ COMMANDS = {
 }
 
 class ShopBotConsumer(WebsocketConsumer):
-    
+
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json.get('message', '')
-        message_parts = message.split()
 
         response_message = 'Vui lòng nhập `help` để xem danh sách các lệnh có thể sử dụng.'
 
-        if message_parts:
-            command = message_parts[0].lower()
+        command_match = re.match(r'(\w+)', message)
+        if command_match:
+            command = command_match.group(1).lower()
             if command == 'help':
                 response_message = 'Các lệnh hỗ trợ:\n' + '\n'.join(
                     [f'{command} - {params["help"]}' for command, params in COMMANDS.items()])
             elif command in COMMANDS:
                 required_args = COMMANDS[command]['args']
                 if command == 'order':
-                    # Command 'order' không có số lượng tham số cố định, cần kiểm tra số lượng cặp sản phẩm và số lượng
-                    if len(message_parts[2:]) % 2 != 0:
-                        response_message = 'Lệnh `order` yêu cầu các cặp mã sản phẩm và số lượng.'
-                    else:
-                        customer_code = message_parts[1]
-                        orderlines = []
-                        for i in range(2, len(message_parts), 2):
-                            product_code = message_parts[i]
-                            quantity = message_parts[i + 1]
-                            orderlines.append({'product_code': product_code, 'quantity': quantity})
-
-                        task_name = COMMANDS[command]['task']
-                        url = "http://127.0.0.1:8000/home/chatbot/handle-task/"
-                        data = {
-                            'task_name': task_name,
-                            'channel_name': self.channel_name,
-                            'args': [customer_code, orderlines]
-                        }
-                        response = requests.post(url, json=data)
-                        response_message = f'Đã nhận `{message}`, đang xử lý...'
+                    # Sử dụng regex để trích xuất mã khách hàng, mã sản phẩm và số lượng
+                    order_match = re.match(r'order\s+(\w+)\s+(.+)', message)
+                    if order_match:
+                        customer_code = order_match.group(1)
+                        order_details = order_match.group(2)
+                        orderlines = re.findall(r'(\w+)\s+(\d+)', order_details)
+                        if not orderlines:
+                            response_message = 'Lệnh `order` yêu cầu các cặp mã sản phẩm và số lượng.'
+                        else:
+                            orderlines = [{'product_code': code, 'quantity': qty} for code, qty in orderlines]
+                            task_name = COMMANDS[command]['task']
+                            url = "http://127.0.0.1:8000/home/chatbot/handle-task/"
+                            data = {
+                                'task_name': task_name,
+                                'channel_name': self.channel_name,
+                                'args': [customer_code, orderlines]
+                            }
+                            response = requests.post(url, json=data)
+                            response_message = f'Đã nhận `{message}`, đang xử lý...'
                 else:
-                    if len(message_parts[1:]) != required_args:
-                        response_message = f'Lệnh `{command}` yêu cầu {required_args} tham số, vui lòng kiểm tra lại.'
-                    else:
-                        task_name = COMMANDS[command]['task']
-                        # Gửi yêu cầu tới view để xử lý task
-                        url = "http://127.0.0.1:8000/home/chatbot/handle-task/"
-                        data = {
-                            'task_name': task_name,
-                            'channel_name': self.channel_name,
-                            'args': message_parts[1:]
-                        }
-                        response = requests.post(url, json=data)
-                        response_message = f'Đã nhận `{message}`, đang xử lý...'
+                    args_match = re.match(r'\w+\s+(.*)', message)
+                    if args_match:
+                        args = args_match.group(1).split()
+                        if len(args) != required_args:
+                            response_message = f'Lệnh `{command}` yêu cầu {required_args} tham số, vui lòng kiểm tra lại.'
+                        else:
+                            task_name = COMMANDS[command]['task']
+                            url = "http://127.0.0.1:8000/home/chatbot/handle-task/"
+                            data = {
+                                'task_name': task_name,
+                                'channel_name': self.channel_name,
+                                'args': args
+                            }
+                            response = requests.post(url, json=data)
+                            response_message = f'Đã nhận `{message}`, đang xử lý...'
 
         self.send(text_data=json.dumps({
             'message': response_message
         }))
-
 
     def chat_message(self, event):
         message = json.loads(event.get('message', '{}'))
